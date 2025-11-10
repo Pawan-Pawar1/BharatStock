2f6ca609c61d82da985ed6b1a226c1845f3ba80a
pawan pawar <pavanpawar2442005@gmail.com>
2025-11-10 17:10:54 +0530

    footer and hero file updated

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,40 +1,43 @@
 import React from 'react';
-function Hero() {
-    return ( 
-        <section className='container-fluid ' id='supportHero'>
-            <div className='mt-5 p-5' id='supportWraper'>
-                <h4>Support Portal</h4>
-                <a href='#'>Track Tickets</a>
-            </div>
 
-            <div className='row p-5 mt-5' >
-                <div className='col-lg-6 col-md-12 p-5 mt-5' >
-                     <h1 className="fs-3">
+
+function Hero() {
+  return (
+    <section className='container-fluid' id='supportHero'>
+      <div className='mt-5 p-5' id='supportWraper'>
+        <h4>Support Portal</h4>
+        <span className='fake-link'>Track Tickets</span>
+      </div>
+
+      <div className='row p-5 mt-5'>
+        <div className='col-lg-6 col-md-12 p-5 mt-5'>
+          <h1 className="fs-3">
             Search for an answer or browse help topics to create a ticket
           </h1>
+
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-            <a href="#">Track account opening</a> &nbsp;&nbsp;
-          <a href="#">Track segment activation</a>&nbsp;&nbsp;
-          <a href="#">Intraday margins</a>&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#">Kite user manual</a>&nbsp;&nbsp;
-                </div>
-                <div className='col-lg-6 col-md-12 p-5 mt-5' >
 
-                     <h1 className="fs-3">Featured</h1>
+          <span className='fake-link'>Track account opening</span>&nbsp;&nbsp;
+          <span className='fake-link'>Track segment activation</span>&nbsp;&nbsp;
+          <span className='fake-link'>Intraday margins</span>&nbsp;&nbsp;&nbsp;&nbsp;
+          <span className='fake-link'>Kite user manual</span>&nbsp;&nbsp;
+        </div>
+
+        <div className='col-lg-6 col-md-12 p-5 mt-5'>
+          <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="#">Current Takeovers and Delisting - January 2024</a>
+              <span className='fake-link'>Current Takeovers and Delisting - January 2024</span>
             </li>
             <li>
-              <a href="#">Latest Intraday leverages - MIS & CO</a>
+              <span className='fake-link'>Latest Intraday leverages - MIS & CO</span>
             </li>
           </ol>
-                </div>
-                
-            </div>
-        </section>
-     );
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
